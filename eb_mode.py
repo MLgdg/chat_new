f3808d8abb767c75892019b59491625665d0af01
MLgdg <43227841+MLgdg@users.noreply.github.com>
2024-12-04 14:30:28 +0800

    Update eb_mode.py

--- eb_mode.py
+++ eb_mode.py
@@ -4,25 +4,19 @@ import json
 import requests
 import time
 
-def main_eb4(content):
+def main_eb4(messages):
     res = ''
     token = ''
     url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/api6?access_token=" + token
 
     payload = json.dumps({
-        "messages": [
-            {
-                "role": "user",
-                "content": content
-            }
-        ], 
+        "messages": messages,
         "extra_parameters": {"training_task_id": "meg_baiduapp_article_dialog"},
         "max_output_tokens": 2048
     })
     headers = {
         'Content-Type': 'application/json'
     }
-    
     response = requests.request("POST", url, headers=headers, data=payload)
 
     result = json.loads(response.text)
